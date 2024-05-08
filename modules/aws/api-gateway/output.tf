628abaec695ab5547bc960f0c9cce7a962002e51
Ezzio Moreira <enzziom@gmail.com>
2024-05-08 18:26:54 -0300

    [INF-474] feat/add output apigt (#25)

---
+++ modules/aws/api-gateway/output.tf
@@ -0,0 +1,9 @@
+output "apigateway_domain_name" {
+    value = aws_apigatewayv2_domain_name.this[0].domain_name_configuration[0].target_domain_name
+    description = "value of apigateway_domain_name"
+}
+
+output "apigateway_hosted_zone_id" {
+    value = aws_apigatewayv2_domain_name.this[0].domain_name_configuration[0].hosted_zone_id
+    description = "value of apigateway_hosted_zone_id"
+}
