b065da50a2003b23ec0c8c9ef30a44c263c8e95a
Ezzio Moreira <enzziom@gmail.com>
2024-05-20 14:30:23 -0300

    feat: adiciona output sg alb (#43)

--- resources/aws/ecs-cluster/output.tf
+++ resources/aws/ecs-cluster/output.tf
@@ -7,3 +7,8 @@ output "loadbalance" {
   value       = module.loadbalance
   description = "exibe todos atributos do loadbalance"
 }
+
+output "security_group" {
+  value       = aws_security_group.this.id
+  description = "exibe atributos do security group"
+}
