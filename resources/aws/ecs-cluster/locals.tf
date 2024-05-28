fec02ce9a80a90055628ed81244059c4b7932992
Ezzio Moreira <enzziom@gmail.com>
2024-05-28 14:34:41 -0300

    [PPP-1325] feat: cria cluster ECS para production (#52)

--- resources/aws/ecs-cluster/locals.tf
+++ resources/aws/ecs-cluster/locals.tf
@@ -23,12 +23,35 @@ locals {
       loadbalance_type = "application"
       internal         = false
     }
+    production = {
+      # atributos do cluster
+      cluster_name = "production"
+      min_size     = 1
+      max_size     = 2
+      desired_size = 2
+
+      autoscaling_instance_types = [{
+        instance_type     = "t3a.small"
+        weighted_capacity = 1
+        },
+        {
+          instance_type     = "t3a.medium"
+          weighted_capacity = 2
+      }]
+      on_demand_percentage_above_base_capacity = 50             # 100% de instâncias sob demanda
+      spot_allocation_strategy                 = "lowest-price" # menor preço
+      spot_instance_pools                      = 1              # quantidade de instance stop por AZ
+
+      # atributos do loadbalance
+      loadbalance_type = "application"
+      internal         = false
+    }
   }
 
 
   # tags para recursos
   tags = {
-    env           = "development"
+    env           = "${terraform.workspace}"
     service       = "ecs-cluster"
     team          = "devops"
     repository    = "drcash-brazil/drcash_platform_infrastructure"
